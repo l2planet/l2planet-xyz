74d5901bb3b3ce5b843399dd67e0651af54994ff
Berzan <berzan@tutanota.com>
2022-07-08 22:19:43 +0300

    add: Topbar & Menu

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 import Page from "$lib/components/Page.svelte";
+import TopBar from "$lib/components/TopBar.svelte";
 
 </script>
-
+<!-- Index Page starts here -->
 <Page>
+    <TopBar/>
     
 </Page>
