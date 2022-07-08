b189e580567fad5f69cb897292b91c3f8dc98287
Berzan <berzan@tutanota.com>
2022-07-08 22:52:28 +0300

    remove: Menu inside

--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -1,19 +1,12 @@
 <script lang="ts">
     import IconMenu from "./icons/IconMenu.svelte";
-    import Menu from "./Menu.svelte";
-    // isMenuOpen handles how menu screen is displayed
-    let isMenuOpen: boolean = false
 </script>
 
 <header class="sticky top-0 z-30 flex items-center justify-between h-16 px-7 bg-white/20 backdrop-blur-lg">
     <!-- This element is like the brand logo of the website -->
     <a class="font-extrabold text-2xl" href="/">L2 Planet</a>
     <!-- This button opens and closes the menu screen -->
-    <button on:click={() => isMenuOpen = !isMenuOpen} class="md:hidden p-2 rounded-full hover:bg-neutral-900/10 active:bg-neutral-700/20 duration-200">
+    <button on:click class="md:hidden p-2 rounded-full hover:bg-neutral-900/10 active:bg-neutral-700/20 duration-200">
         <IconMenu/> <p hidden>menu</p>
     </button>
-    <!-- If button is clicked display the menu screen -->
-    {#if isMenuOpen}
-        <Menu/>
-    {/if}
 </header>
