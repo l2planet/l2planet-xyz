e9b5f406b7e900ac452b0c904c7cb12b38397f39
Berzan <berzan@tutanota.com>
2022-07-08 22:55:28 +0300

    add: Menu

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 import Main from "$lib/components/Main.svelte";
+import Menu from "$lib/components/Menu.svelte";
 import Page from "$lib/components/Page.svelte";
 import TopBar from "$lib/components/TopBar.svelte";
 
+// isMenuOpen handles how menu screen is displayed
+let isMenuOpen: boolean = false
 </script>
+
 <!-- Index Page starts here -->
 <Page>
-    <TopBar/>
+    <TopBar on:click={() => isMenuOpen = !isMenuOpen}/> <!-- If menu button is clicked, isMenuOpen value will be changed -->
+    <Menu isOpen={isMenuOpen}/>
+    <!-- The contents will be right here -->
     <Main>
-        
+        dsfdslkejwWWWWWWWWWWWWWWWWWWWWWWWWW
     </Main>
 </Page>
