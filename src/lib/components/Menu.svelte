bd176ed4cab623c7b36694f0f380221fc4c8a434
Berzan <berzan@tutanota.com>
2022-07-09 21:28:11 +0300

    remove: animations for mobile devices

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -9,12 +9,12 @@
     <div class="animated md:hidden fixed left-0 top-16 flex flex-col p-4 w-full min-h-[calc(100vh-4rem)] bg-white/50 backdrop-blur-xl select-none">
         <!-- Each anchor element represents a section of the website -->
         <nav class="flex flex-col min-h-80 gap-y-2 overflow-y-auto h-[calc(100vh-6rem)]">
-            <a href="/" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Home Page</a>
-            <a href="/solutions" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Solutions</a>
-            <a href="/blog" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Blog</a>
-            <a href="/stats" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Stats</a>
-            <a href="/story" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Our Story</a>
-            <a href="/sponsor" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Sponsor Us</a>
+            <a href="/" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Home Page</a>
+            <a href="/solutions" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Solutions</a>
+            <a href="/blog" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Blog</a>
+            <a href="/stats" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Stats</a>
+            <a href="/story" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Our Story</a>
+            <a href="/sponsor" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Sponsor Us</a>
         </nav>
     </div>
 {/if}
