776f87ec2e9985545d2731948aac9333536833e0
Berzan <berzan@tutanota.com>
2022-07-09 09:54:04 +0300

    remove: transparency

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -6,7 +6,7 @@
 <!-- If isOpen == true, it displays the menu screen, otherwise not -->
 {#if isOpen}
     <!-- Menu screen will only be displayed on mobile screens --> 
-    <div class="animated md:hidden fixed left-0 top-16 flex flex-col w-full min-h-[calc(100vh-4rem)] bg-white/50 backdrop-blur-lg">
+    <div class="animated md:hidden fixed left-0 top-16 flex flex-col w-full min-h-[calc(100vh-4rem)] bg-white">
         <!-- Each anchor element represents a section of the website -->
         <nav class="flex flex-col min-h-80 p-7 gap-y-2">
             <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Home Page</a>
--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -2,7 +2,7 @@
     import IconMenu from "./icons/IconMenu.svelte";
 </script>
 
-<header class="sticky top-0 z-30 flex items-center justify-between h-16 px-7 bg-white/20 backdrop-blur-lg">
+<header class="sticky top-0 z-30 flex items-center justify-between h-16 px-7 bg-white">
     <!-- This element is like the brand logo of the website -->
     <a class="font-extrabold text-2xl" href="/">L2 Planet</a>
     <!-- This button opens and closes the menu screen -->
