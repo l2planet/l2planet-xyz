0066082e5e798b9e52745fdf55d4b81afb5b6944
Berzan <berzan@tutanota.com>
2022-07-09 09:01:24 +0300

    add: nav element

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -6,12 +6,14 @@
 <!-- If isOpen == true, it displays the menu screen, otherwise not -->
 {#if isOpen}
     <!-- Menu screen will only be displayed on mobile screens --> 
-    <div class="animated md:hidden fixed left-0 top-16 flex flex-col p-7 gap-y-2 w-full min-h-[calc(100vh-4rem)] bg-white/50 backdrop-blur-lg">
+    <div class="animated md:hidden fixed left-0 top-16 flex flex-col w-full min-h-[calc(100vh-4rem)] bg-white/50 backdrop-blur-lg">
         <!-- Each anchor element represents a section of the website -->
-        <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Home Page</a>
-        <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Statistics</a>
-        <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Our Story</a>
-        <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Sponsor Us</a>
+        <nav class="flex flex-col min-h-80 p-7 gap-y-2">
+            <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Home Page</a>
+            <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Statistics</a>
+            <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Our Story</a>
+            <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Sponsor Us</a>
+        </nav>
     </div>
 {/if}
 
