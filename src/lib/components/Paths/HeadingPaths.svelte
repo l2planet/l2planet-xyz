d7ce2afab791aa4f86107ef047b1596c7fd41ef0
Berzan <berzan@tutanota.com>
2022-07-09 20:44:11 +0300

    change: font sizes

--- src/lib/components/Paths/HeadingPaths.svelte
+++ src/lib/components/Paths/HeadingPaths.svelte
@@ -1,4 +1,4 @@
 <!-- Heading for `GridPaths` component -->
-<h1 class="font-bold text-4xl sm:text-5xl lg:text-6xl">
+<h1 class="font-bold text-5xl md:text-6xl">
     Pick a Path
 </h1>
--- src/lib/components/Paths/ParagraphPaths.svelte
+++ src/lib/components/Paths/ParagraphPaths.svelte
@@ -1,4 +1,4 @@
 <!-- Paragraph for `GridPaths` component -->
-<p class="font-semibold sm:text-lg md:text-xl lg:text-2xl text-neutral-900/70">
+<p class="font-semibold text-lg sm:text-xl md:text-2xl text-neutral-900/70">
     Pick your path on the road of L2s
 </p>
