80caa3c04a7a25467cacd759c7dd1dab674c9919
Berzan <berzan@tutanota.com>
2022-07-09 19:53:37 +0300

    change: styling for mobile devices

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -6,13 +6,13 @@
 <!-- If isOpen == true, it displays the menu screen, otherwise not -->
 {#if isOpen}
     <!-- Menu screen will only be displayed on mobile screens --> 
-    <div class="animated md:hidden fixed left-0 top-16 flex flex-col w-full min-h-[calc(100vh-4rem)] bg-white/50 backdrop-blur-xl">
+    <div class="animated md:hidden fixed left-0 top-16 flex flex-col p-4 w-full min-h-[calc(100vh-4rem)] bg-white/50 backdrop-blur-xl select-none">
         <!-- Each anchor element represents a section of the website -->
-        <nav class="flex flex-col min-h-80 p-7 gap-y-2">
-            <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Home Page</a>
-            <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Statistics</a>
-            <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Our Story</a>
-            <a href="/" class="flex items-center h-16 font-semibold text-3xl text-neutral-600">Sponsor Us</a>
+        <nav class="flex flex-col min-h-80 gap-y-2">
+            <a href="/" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Home Page</a>
+            <a href="/" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Statistics</a>
+            <a href="/" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Our Story</a>
+            <a href="/" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50 active:text-violet-600/50 active:bg-neutral-900/10 duration-200 active:scale-95">Sponsor Us</a>
         </nav>
     </div>
 {/if}
--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -6,7 +6,7 @@
     <!-- This element is like the brand logo of the website -->
     <a class="font-extrabold text-2xl" href="/">L2 Planet</a>
     <!-- This button opens and closes the menu screen -->
-    <button on:click class="md:hidden p-2 rounded-full hover:bg-neutral-900/10 active:bg-neutral-700/20 duration-200">
+    <button on:click class="md:hidden p-2 rounded-full active:bg-neutral-700/10 active:scale-90 duration-200">
         <IconMenu/> <p hidden>menu</p>
     </button>
 </header>
