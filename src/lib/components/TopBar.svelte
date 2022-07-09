87732fad641147e174eba698c615a76289ebccfe
Berzan <berzan@tutanota.com>
2022-07-09 19:32:56 +0300

    add: blur & transparency

--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -2,7 +2,7 @@
     import IconMenu from "./icons/IconMenu.svelte";
 </script>
 
-<header class="sticky top-0 z-30 flex items-center justify-between h-16 px-7 bg-white">
+<header class="sticky top-0 z-30 flex items-center justify-between h-16 px-7 bg-white/50 backdrop-blur-xl">
     <!-- This element is like the brand logo of the website -->
     <a class="font-extrabold text-2xl" href="/">L2 Planet</a>
     <!-- This button opens and closes the menu screen -->
