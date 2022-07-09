1173a5f411182a1d7d1020dd41cda90be74bb2c2
Berzan <berzan@tutanota.com>
2022-07-09 22:04:40 +0300

    remove: 'Home Page' anchor element

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -9,10 +9,9 @@
     <div class="animated md:hidden fixed left-0 top-16 flex flex-col p-4 w-full min-h-[calc(100vh-4rem)] bg-white/50 backdrop-blur-xl select-none">
         <!-- Each anchor element represents a section of the website -->
         <nav class="flex flex-col min-h-80 gap-y-2 overflow-y-auto h-[calc(100vh-6rem)]">
-            <a href="/" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Home Page</a>
             <a href="/l2 solutions" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">L2 Solutions</a>
-            <a href="/blog" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Blog</a>
             <a href="/stats" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Stats</a>
+            <a href="/blog" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Blog</a>
             <a href="/story" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Our Story</a>
             <a href="/sponsor" class="flex items-center p-3 rounded-2xl h-16 font-semibold text-3xl text-neutral-900/50">Sponsor Us</a>
         </nav>
