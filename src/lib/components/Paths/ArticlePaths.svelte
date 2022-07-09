d5946e5976eb710dfc20c0d54226f4e29b8aadc6
Berzan <berzan@tutanota.com>
2022-07-09 21:12:37 +0300

    change: mobile styling

--- src/lib/components/Paths/ArticlePaths.svelte
+++ src/lib/components/Paths/ArticlePaths.svelte
@@ -1,4 +1,4 @@
 <!-- `ArticlePath` component will include all components related to `Paths` -->
-<article class="flex flex-col items-center gap-y-8 p-4 md:p-6">
+<article class="flex flex-col items-center gap-y-8 p-4 sm:p-6">
     <slot/>
 </article>
--- src/lib/components/Paths/GridPaths.svelte
+++ src/lib/components/Paths/GridPaths.svelte
@@ -1,4 +1,4 @@
 <!-- Grid container for `CardPaths` components -->
-<div class="w-full max-w-6xl h-[calc(100vw-3.5rem)] md:h-[calc(25vw-1.875rem)] md:max-h-[16.125rem] grid grid-cols-2 md:grid-cols-4 gap-6">
+<div class="w-full max-w-6xl h-[calc(100vw-3.5rem)] sm:h-[calc(100vw-4.5rem)] md:h-[calc(25vw-1.875rem)] md:max-h-[16.125rem] grid grid-cols-2 md:grid-cols-4 gap-6">
     <slot/>
 </div>
