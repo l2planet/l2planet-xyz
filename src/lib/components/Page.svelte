8081826c9f51f925ab425d78a0bd9da787ac4cde
Berzan <berzan@tutanota.com>
2022-07-10 18:40:49 +0300

    change: color & background color

--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -1,3 +1,3 @@
-<div class="flex flex-col min-h-screen bg-white text-neutral-900">
+<div class="flex flex-col min-h-screen bg-white text-neutral-900 dark:bg-gri-800 dark:text-white">
     <slot/>
 </div>
