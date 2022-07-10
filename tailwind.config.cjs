5ddf8be6d4faeaa79d0d228c5607ebdd3459e835
Berzan <berzan@tutanota.com>
2022-07-10 18:07:51 +0300

    change: darkMode & extend property

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.svelte'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gri': {
+          '900': '#222222',
+          '800': "#2c2d30",
+          '700': "#3a3b3f",
+          '600': "#484a4f",
+          '500': "#56585e",
+        }
+      },
+    },
   },
   plugins: [],
 }
