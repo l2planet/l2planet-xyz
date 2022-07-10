1eb350208eeb2e4e5bbe27e897269f04a429b6cd
Berzan <berzan@tutanota.com>
2022-07-10 19:21:08 +0300

    add: adapter-cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import cloudflareAdapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter()
+		adapter: cloudflareAdapter()
 	}
 };
 
