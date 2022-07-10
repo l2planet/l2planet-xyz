fe70aae0646a4718d4764a04958c3565901aa32a
Berzan <berzan@tutanota.com>
2022-07-10 19:10:28 +0300

    change: font style

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
-<footer class="flex flex-col bg-[#eeeeee] dark:bg-gri-900 py-16 px-7">
+<footer class="flex flex-col bg-[#eeeeee] dark:bg-gri-900 py-16 px-7 font-bold text-3xl">
     ©L2 Planet
 </footer>
