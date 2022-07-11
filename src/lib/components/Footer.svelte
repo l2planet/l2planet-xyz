959ba10eccdbe9c30d9b35a4b0b67f0d421a7582
Berzan <berzan@tutanota.com>
2022-07-11 18:48:01 +0300

    add: Footer objects

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,30 @@
-<footer class="flex flex-col bg-[#eeeeee] dark:bg-gri-900 py-16 px-7 font-bold text-3xl">
-    ©L2 Planet
+<footer class="flex items-center justify-center px-8 py-16 bg-[#eeeeee] dark:bg-gri-900">
+    <div class="flex flex-col md:flex-row justify-between w-full max-w-5xl gap-y-16">
+        <div class="flex flex-col gap-y-4">
+            <h3 class="font-bold text-3xl">L2 Planet</h3>
+            <p class="font-semibold text-sm">© L2 Planet Labs.</p>
+        </div>
+        <div class="flex flex-wrap items-start justify-start gap-x-24 gap-y-12">
+            <div class="flex flex-col gap-y-3">
+                <h4 class="font-bold text-lg">Social Media</h4>
+                <ul class="flex flex-col items-start gap-y-2 font-semibold text-[.9375rem] text-neutral-900/60 dark:text-white/60">
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">Twitter</a></li>
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">Discord</a></li>
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">Youtube</a></li>
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">Instagram</a></li>
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">TikTok</a></li>
+                </ul>
+            </div>
+            <div class="flex flex-col gap-y-3">
+                <h4 class="font-bold text-lg">Social Media</h4>
+                <ul class="flex flex-col items-start gap-y-2 font-semibold text-[.9375rem] text-neutral-900/60 dark:text-white/60">
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">Twitter</a></li>
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">Discord</a></li>
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">Youtube</a></li>
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">Instagram</a></li>
+                    <li class="md:hover:text-fuchsia-600 md:dark:hover:text-fuchsia-400"><a href="/">TikTok</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
