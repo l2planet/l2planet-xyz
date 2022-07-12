9cf0f9e5dcd16a42396ea4f4a7bdab0cf04efa4c
Berzan <berzan@tutanota.com>
2022-07-12 10:51:01 +0300

    add: pages

---
+++ src/routes/tezos/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Footer from "$lib/components/Footer.svelte";
+import Main from "$lib/components/Main.svelte";
+import Page from "$lib/components/Page.svelte";
+import TopBar from "$lib/components/TopBar/TopBar.svelte";
+
+</script>
+
+<!-- Tezos Page starts here -->
+<Page>
+    <TopBar/>
+    <!-- The contents will be right here -->
+    <Main>
+        Tezos...
+    </Main>
+    <Footer/>
+</Page>
