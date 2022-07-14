0608d1223ec49a85491c2988cded8eec26fc28b5
Berzan <berzan@tutanota.com>
2022-07-14 14:15:47 +0300

    change: style

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -1,4 +1,4 @@
 <!-- main element is where contents will be placed -->
-<main class="flex flex-col items-center flex-1 w-full max-w-6xl p-4 sm:p-6 gap-y-16">
+<main class="flex flex-col text-center flex-1 w-full max-w-6xl p-4 sm:p-6 gap-y-16">
     <slot/>
 </main>
