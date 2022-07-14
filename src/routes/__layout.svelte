01f12d0ec7fe88fdc22dbf9e2bc6c42d0462fc94
Berzan <berzan@tutanota.com>
2022-07-14 13:34:55 +0300

    optimize: font performance

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,6 +11,8 @@ import Footer from '$lib/components/Footer.svelte';
         theme.init()
     })
 </script>
+
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"/>
 <Page>
     <TopBar/>
     <Main>
