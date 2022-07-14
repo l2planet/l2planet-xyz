d6ecd585a92ebfdb6926cecd9ba2f206fbeac1a0
Berzan <berzan@tutanota.com>
2022-07-14 14:16:05 +0300

    remove: unused imports

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,23 +4,21 @@ import GridAbout from "$lib/components/About/GridAbout.svelte";
 import ParagraphAbout from "$lib/components/About/ParagraphAbout.svelte";
 import H1 from "$lib/components/H1.svelte";
 import H2 from "$lib/components/H2.svelte";
-import Section from "$lib/components/Section.svelte";
 
 </script>
 
 <!-- About Page starts here -->
-<Section>
-    <div class="flex flex-col gap-y-3 sm:gap-y-4">
-        <H1>About Us</H1>
-        <ParagraphAbout>L2 Planet is a community-driven L2 newsletter project which is run by volunteers</ParagraphAbout>
-    </div>
-    <div class="flex flex-col gap-y-8 sm:gap-y-12">
-        <H2>What we do</H2>
-        <GridAbout>
-            <CardAbout title="Newsletter">People can easily track the state of L2s by reading</CardAbout>
-            <CardAbout title="Newsletter">People can easily track the state of L2s by reading</CardAbout>
-            <CardAbout title="Newsletter">People can easily track the state of L2s by reading</CardAbout>
-            <CardAbout title="Newsletter">People can easily track the state of L2s by reading</CardAbout>
-        </GridAbout>
-    </div>
-</Section>
+
+<div class="flex flex-col gap-y-3 sm:gap-y-4">
+    <H1>About Us</H1>
+    <ParagraphAbout>L2 Planet is a community-driven L2 newsletter project which is run by volunteers</ParagraphAbout>
+</div>
+<div class="flex flex-col gap-y-8 sm:gap-y-12">
+    <H2>What we do</H2>
+    <GridAbout>
+        <CardAbout title="Newsletter">People can easily track the state of L2s by reading</CardAbout>
+        <CardAbout title="Newsletter">People can easily track the state of L2s by reading</CardAbout>
+        <CardAbout title="Newsletter">People can easily track the state of L2s by reading</CardAbout>
+        <CardAbout title="Newsletter">People can easily track the state of L2s by reading</CardAbout>
+    </GridAbout>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,25 +11,22 @@ import IconFantom from "$lib/components/icons/IconFantom.svelte";
 import IconSysCoin from "$lib/components/icons/IconSysCoin.svelte";
 import IconMoonBeam from "$lib/components/icons/IconMoonBeam.svelte";
 import H1 from "$lib/components/H1.svelte";
-import Sections from "$lib/components/Section.svelte";
-import Section from "$lib/components/Section.svelte";
 
 </script>
 
 <!-- Index Page starts here -->
-<Section>
-    <div class="flex flex-col gap-y-8">
-        <H1>Pick a Path</H1>
-        <ParagraphChains>Pick your path on the road of L2s</ParagraphChains>
-        <GridChains>
-            <CardChains name="Ethereum" href="/ethereum"> <IconEthereum/> </CardChains>
-            <CardChains name="Bitcoin" href="/bitcoin"> <IconBitcoin/> </CardChains>
-            <CardChains name="Cardano" href="/cardano"> <IconCardano/> </CardChains>
-            <CardChains name="Tezos" href="/tezos"> <IconTezos/> </CardChains>
-            <CardChains name="Mina" href="/mina"> <IconMina/> </CardChains>
-            <CardChains name="Fantom" href="/fantom"> <IconFantom/> </CardChains>
-            <CardChains name="MoonBeam" href="/moonbeam"> <IconMoonBeam/> </CardChains>
-            <CardChains name="SysCoin" href="/syscoin"> <IconSysCoin/> </CardChains>
-        </GridChains>
-    </div>
-</Section>
+
+<div class="flex flex-col gap-y-8">
+    <H1>Pick a Path</H1>
+    <ParagraphChains>Pick your path on the road of L2s</ParagraphChains>
+    <GridChains>
+        <CardChains name="Ethereum" href="/ethereum"> <IconEthereum/> </CardChains>
+        <CardChains name="Bitcoin" href="/bitcoin"> <IconBitcoin/> </CardChains>
+        <CardChains name="Cardano" href="/cardano"> <IconCardano/> </CardChains>
+        <CardChains name="Tezos" href="/tezos"> <IconTezos/> </CardChains>
+        <CardChains name="Mina" href="/mina"> <IconMina/> </CardChains>
+        <CardChains name="Fantom" href="/fantom"> <IconFantom/> </CardChains>
+        <CardChains name="MoonBeam" href="/moonbeam"> <IconMoonBeam/> </CardChains>
+        <CardChains name="SysCoin" href="/syscoin"> <IconSysCoin/> </CardChains>
+    </GridChains>
+</div>
