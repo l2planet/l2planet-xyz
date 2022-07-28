7d436e0f520ff4ba6844451721254161ed9ce62d
Berzan <berzan@tutanota.com>
2022-07-28 13:29:14 +0300

    change: style

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,13 +1,13 @@
 <footer class="flex items-center justify-center w-full border-t border-slate-200 dark:border-slate-700 bg-slate-100 dark:bg-slate-800">
-    <div class="flex flex-col md:flex-row w-full max-w-6xl px-8 py-16 gap-y-14">
+    <div class="flex flex-col md:flex-row w-full max-w-5xl px-8 py-16 gap-y-14">
         <div class="flex flex-col gap-y-4 w-full md:w-1/2">
-            <h3 class="font-bold text-3xl">L2 Planet</h3>
-            <p class="font-semibold text-sm">© L2 Planet Labs.</p>
+            <h3 class="font-bold text-4xl">L2 Planet</h3>
+            <p class="font-semibold">© L2 Planet Labs.</p>
         </div>
         <div class="flex flex-wrap justify-between w-full md:w-1/2 gap-12 pr-32 md:pr-0">
             <div class="flex flex-col gap-y-3">
                 <h4 class="font-bold text-lg">Social Media</h4>
-                <ul class="flex flex-col items-start gap-y-2 font-semibold text-[.9375rem] text-dark/70 dark:text-lite/70">
+                <ul class="flex flex-col items-start gap-y-2 font-semibold text-dark/70 dark:text-lite/70">
                     <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Twitter</a></li>
                     <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Discord</a></li>
                     <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Youtube</a></li>
@@ -26,13 +26,11 @@
                 </ul>
             </div>
             <div class="flex flex-col gap-y-3">
-                <h4 class="font-bold text-lg">Social Media</h4>
-                <ul class="flex flex-col items-start gap-y-2 font-semibold text-[.9375rem] text-dark/70 dark:text-lite/70">
-                    <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Twitter</a></li>
-                    <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Discord</a></li>
-                    <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Youtube</a></li>
-                    <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Instagram</a></li>
-                    <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">TikTok</a></li>
+                <h4 class="font-bold text-lg">DAO</h4>
+                <ul class="flex flex-col items-start gap-y-2 font-semibold text-[1rem] text-dark/70 dark:text-lite/70">
+                    <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Writers</a></li>
+                    <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Join Us</a></li>
+                    <li class="hover:text-sky-600 dark:hover:text-sky-400 duration-100"><a href="/">Donate</a></li>
                 </ul>
             </div>
         </div>
