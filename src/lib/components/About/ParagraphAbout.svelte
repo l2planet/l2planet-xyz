4d29afdcad30ca8bb1b4e12d6de787b80810c8ca
Berzan <berzan@tutanota.com>
2022-07-28 14:01:08 +0300

    remove: unused components

--- src/lib/components/About/ParagraphAbout.svelte
+++
@@ -1,3 +0,0 @@
-<p class="font-semibold text-l sm:text-lg md:text-xl text-slate-900/70 dark:text-white/70">
-    <slot/>
-</p>
--- src/lib/components/Chains/ParagraphChains.svelte
+++
@@ -1,4 +0,0 @@
-<!-- Paragraph for `GridPaths` component -->
-<p class="font-semibold text-lg sm:text-xl md:text-2xl text-slate-900/70 dark:text-white/70">
-    <slot/>
-</p>
