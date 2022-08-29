2561b6aef19fcb47c71b6999a28a488265cb8346
Berzan <berzan@tutanota.com>
2022-08-29 18:49:17 +0300

    update: next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  experimental: {
+    images: {
+        unoptimized: true
+    }
+  }
 }
 
 module.exports = nextConfig
